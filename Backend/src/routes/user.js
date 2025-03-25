aa9f65b8f1a84a1b9e1b0020f24ee1d60ae06184
Akshay Kumar Sakinam <sakinamakshay@gmail.com>
2025-03-25 12:40:45 +0530

    updated

--- Backend/src/routes/user.js
+++ Backend/src/routes/user.js
@@ -1,10 +1,9 @@
-
 const express = require("express");
 const userandadminauth = require("../middlewares/userandadminauth");
 const userRouter = express.Router();
 
 // This route returns the authenticated user's data
-userRouter.get("/user", userandadminauth(), (req, res) => {
+userRouter.get("/", userandadminauth(), (req, res) => {
   res.json({ user: req.user });
 });
 
--- project/src/axiosConfig.jsx
+++ project/src/axiosConfig.jsx
@@ -7,7 +7,45 @@ const BASE_URL = window.location.hostname === "localhost"
 const instance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
 });
+
+// Add request interceptor for debugging
+instance.interceptors.request.use(
+  (config) => {
+    console.log("Making request to:", config.url);
+    return config;
+  },
+  (error) => {
+    console.error("Request Error:", error);
+    return Promise.reject(error);
+  }
+);
+
+// Add response interceptor for debugging
+instance.interceptors.response.use(
+  (response) => {
+    console.log("Response received:", response.status);
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      console.error("Response Error:", {
+        status: error.response.status,
+        data: error.response.data,
+        url: error.config.url
+      });
+    } else if (error.request) {
+      console.error("Request Error:", error.request);
+    } else {
+      console.error("Error:", error.message);
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
  
